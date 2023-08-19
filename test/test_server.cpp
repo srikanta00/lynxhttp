229f3eb66907be6a6e7a604b37690920a905d8d8
srikanta00 <srikanta.singh222@gmail.com>
2023-08-19 20:57:27 +0530

    Minor change.

--- test/test_server.cpp
+++ test/test_server.cpp
@@ -1,5 +1,5 @@
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Hello
+#define BOOST_TEST_MODULE lynxhttp_test
 
 #include <boost/test/unit_test.hpp>
 
@@ -14,15 +14,15 @@ namespace lynxserver = lynxhttp::server;
 
 BOOST_AUTO_TEST_SUITE(test_lynxserver)
 
-BOOST_AUTO_TEST_CASE(test1)
+BOOST_AUTO_TEST_CASE(server_curl_basic)
 {
     BOOST_TEST_MESSAGE("Creating server with default parameters: ");
     lynxserver::server srv;
     
     srv.handle("/", [](const lynxserver::request::ptr req, const lynxserver::response::ptr resp){
         BOOST_TEST_MESSAGE("Data received: " << req->body());
-        BOOST_CHECK_EQUAL(req->body(), "hello");
-        resp->send(200, "response");
+        BOOST_CHECK_EQUAL(req->body(), "It is a request.");
+        resp->send(200, "It is a response.");
     });
 
     std::thread thread1([&srv](){
@@ -32,9 +32,9 @@ BOOST_AUTO_TEST_CASE(test1)
     BOOST_TEST_MESSAGE("Waiting for a second...");
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    auto result = exec("curl -X POST \"127.0.0.1:80?k1=v1&k2=v2\" -d 'hello'");
+    auto result = exec("curl -X POST \"127.0.0.1:80?k1=v1&k2=v2\" -d 'It is a request.'");
     BOOST_TEST_MESSAGE("Response received: " << result);
-    BOOST_CHECK_EQUAL(result, "response");
+    BOOST_CHECK_EQUAL(result, "It is a response.");
 
     BOOST_TEST_MESSAGE("Waiting for a second...");
     std::this_thread::sleep_for(std::chrono::seconds(1));
